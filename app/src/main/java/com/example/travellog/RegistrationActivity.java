3c43b053cc04293c46a1a52afcce5f19cc95099c
StudentH <S96841@haklaiph.org>
2025-09-04 14:52:13 +0300

    expended

--- app/src/main/java/com/example/travellog/RegistrationActivity.java
+++ app/src/main/java/com/example/travellog/RegistrationActivity.java
@@ -1,6 +1,10 @@
 package com.example.travellog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.travellog.utils.RegistrationManager;
+
 public class RegistrationActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegistrationActivity";
+    protected EditText emailEditText;
+    protected EditText passwordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,33 @@ public class RegistrationActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
     }
+    private void registerButtonClick() {
+        Log.d(TAG, "Register button clicked");
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        RegistrationManager registrationManager = new RegistrationManager(RegistrationActivity.this);
+        registrationManager.startRegistration(
+                emailEditText.getText().toString(),
+                passwordEditText.getText().toString(),
+                new RegistrationManager.OnResultCallback(){
+                    @Override
+                    public void onResult(boolean success, String message) {
+                        if (success) {
+                            Toast.makeText(RegistrationActivity.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(RegistrationActivity.this, "Registration failed: " + message, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
 }
